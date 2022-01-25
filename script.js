3eb7b758559d4848031311e2edef101e383229b6
Julian Graells <juliancanna2901@gmail.com>
2022-01-25 18:49:55 -0300

    alerts de js

---
+++ script.js
@@ -0,0 +1,7 @@
+const SALUDO = "Hola Navegadores!"
+alert (SALUDO)
+let nombreNavegador = prompt ("¿Cuál es tu nombre?")
+alert ("Bienvenido/a" + nombreNavegador)
+let cantidadCompras = Number (prompt("Cuantas compras haces por año?"))
+let cantidadCompras2032 = cantidadCompras * 10
+alert ("En 2032 vas a haber hecho aproximadamente" + cantidadCompras2032 + "compras")
