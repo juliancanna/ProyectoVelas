d1d9099cbb069a25cd587c3954a26ef8b270208a
Julian Graells <juliancanna2901@gmail.com>
2022-01-25 19:00:39 -0300

    arreglamos let cantidadCompras

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const SALUDO = "Hola Navegadores!"
 alert (SALUDO)
 let nombreNavegador = prompt ("¿Cuál es tu nombre?")
 alert ("Bienvenido/a" + nombreNavegador)
-let cantidadCompras = Number (prompt("Cuantas compras haces por año?"))
+let cantidadCompras = Number (prompt("Cuantas compras online haces por año?"))
 let cantidadCompras2032 = cantidadCompras * 10
 alert ("En 2032 vas a haber hecho aproximadamente" + cantidadCompras2032 + "compras")
