9e339ca94e51ab4321f5a589f3f2855c01ec474c
Julian Graells <juliancanna2901@gmail.com>
2022-02-02 14:26:06 -0300

    js modificacion

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ alert ("Bienvenido/a" + nombreNavegador)
 let cantidadCompras = Number (prompt("Cuantas compras haces por año?"))
 let cantidadCompras2032 = cantidadCompras * 10
 alert ("En 2032 vas a haber hecho aproximadamente" + cantidadCompras2032 + "compras")
+
+//en lugar de alert se puede usar Console.log
+//parseInt usa el numero entero ParseFloat usa el numero en su totalidad
