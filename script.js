7bc34f3bd443b4e865f750a9a547737ccbf058c0
Julian Graells <juliancanna2901@gmail.com>
2022-02-07 19:19:01 -0300

    formateo con beauttify

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ alert ("En 2032 vas a haber hecho aproximadamente" + cantidadCompras2032 + "comp
 //parseInt usa el numero entero ParseFloat usa el numero en su totalidad
 */
 
-for(let i = 0; i<6; i++) {
+for (let i = 0; i < 6; i++) {
     console.log(i)
 }
 
-let acumulador = 0; 
-for(let i = 0; i<10; i++) { // 0 1 2 3 4 5 6 7 8 9
-  let nota = parseInt (prompt("Ingrese una nota"))
-  acumulador += nota // acumulador = acumulador + nota
+let acumulador = 0;
+for (let i = 0; i < 10; i++) { // 0 1 2 3 4 5 6 7 8 9
+    let nota = parseInt(prompt("Ingrese una nota"))
+    acumulador += nota // acumulador = acumulador + nota
 
 }
 
@@ -33,23 +33,24 @@ console.log (cantidadVelas * precioVela) */
 
 let cantidadVelas
 let valor = 300
-do { cantidadVelas = prompt("Cuantas velas desea comprar?");
-    console.log (cantidadVelas);
+do {
+    cantidadVelas = prompt("Cuantas velas desea comprar?");
+    console.log(cantidadVelas);
     if (cantidadVelas == "") {
-        prompt ("Ingrese un valor")
+        prompt("Ingrese un valor")
     }
     if (cantidadVelas < 0) {
-        prompt ("Ingrese un valor positivo")
+        prompt("Ingrese un valor positivo")
     }
 } while (cantidadVelas == "" || cantidadVelas < 0)
 
-alert (`El valor total es  ${valor * cantidadVelas}`)
-   
+alert(`El valor total es  ${valor * cantidadVelas}`)
+
 let num2
 let num1
 num1 = parseInt(prompt("Ingrese valor total"))
 num2 = parseInt(prompt("En cuantas cuotas quiere abonar?"))
 const division = (num1, num2) => num1 / num2
-console.log(division(num1,num2))
+console.log(division(num1, num2))
 
 alert(`El valor de cada cuota es de ${num1 / num2}`)
